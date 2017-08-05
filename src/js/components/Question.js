dc4a5d973b5c40bb47d14b8ec75793eadd096da8
florn <flapsnms@gmail.com>
2017-08-05 15:08:40 +0100

    button counter function

--- src/js/components/Question.js
+++ src/js/components/Question.js
@@ -7,8 +7,15 @@ export default class Question extends React.Component {
   constructor(props) {
     super(props);
     this.state = {questionVersions: ['Solve the following integral: $$ \\int_{1}^{0} x^2  $$ Please show all your work.',
-    'Solve the following integral: $$ \\int_{2}^{3} x^6  $$ Please show all your work.']};
+    'Solve the following integral: $$ \\int_{2}^{3} x^6  $$ Please show all your work.'],
+    questionSelector: 0,
+    versionSelector: 0};
   }
+  
+  handleNewQClick() {    
+    alert('Get new question button pressed.');
+  }
+  
   render() {
     return (
       <div>
@@ -23,7 +30,5 @@ export default class Question extends React.Component {
       </div>
     );
   }
-  handleNewQClick() {
-    alert('Get new question button pressed.');
-  }
+
 }
