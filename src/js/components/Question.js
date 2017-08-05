e4020b4379949eeefa4b687a42a9427fc3b45adb
Donncha O'Toole <dnncha@gmail.com>
2017-08-05 14:02:40 +0100

    question cleaning

--- src/js/components/Question.js
+++ src/js/components/Question.js
@@ -5,8 +5,7 @@ export default class Question extends React.Component {
   render() {
     return (
       <div>
-      <p>Define how many apples John has where Mary is cycling away from a giant spinning apple at a rate of 5 m/s and where the apple is red. </p>
-      <div className="maths">{'Solve the following integral: $$ \\int_{a}^{b} x^2  $$ Please show all your work.'}</div>
+        <div className="maths">{'Solve the following integral: $$ \\int_{a}^{b} x^2  $$ Please show all your work.'}</div>
       </div>
     );
   }
