fe9253dea6fc45dd6b8fb42733fec4c20a73a41b
Donncha O'Toole <dnncha@gmail.com>
2017-08-05 15:05:01 +0100

    button props

--- src/js/components/PreviousButton.js
+++ src/js/components/PreviousButton.js
@@ -1,12 +1,9 @@
 import React from "react";
 
 export default class PreviousButton extends React.Component {  
-  getPrevious() {
-    console.log('Previous button pressed.');
-  }
   render() {
     return (
-      <button onClick={this.getPrevious} class="btn btn-default">Previous</button>
+      <button onClick={this.props.onClick} class="btn btn-default">Previous</button>
     );
   }
 }
--- src/js/components/Question.js
+++ src/js/components/Question.js
@@ -6,24 +6,33 @@ import PreviousButton from './PreviousButton';
 export default class Question extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {questionVersions: ['Solve the following integral: $$ \\int_{1}^{0} x^2  $$ Please show all your work.',
-    'Solve the following integral: $$ \\int_{2}^{3} x^6  $$ Please show all your work.']};
+    this.state = {
+      questionVersions: ['Solve the following integral: $$ \\int_{1}^{0} x^2  $$ Please show all your work.',
+      'Solve the following integral: $$ \\int_{2}^{3} x^6  $$ Please show all your work.']
+    };
   }
+  handleNewQClick() {
+    console.log('Get new question button pressed.');
+  }
+  handleNextBtnClick() {
+    console.log('Next button clicked.');
+  }
+  handlePreviousBtnClick() {
+    console.log('Previous button clicked.');
+  }
+
   render() {
     return (
       <div>
         <div className="maths mt-4">{this.state.questionVersions[0]}</div>
           <div className="btn-group mt-4">
-           <PreviousButton />
-           <NextButton />
+           <PreviousButton onClick={this.handlePreviousBtnClick} />
+           <NextButton onClick={this.handleNextBtnClick} />
          </div>
          <div>
          <NewQuestionButton onClick={this.handleNewQClick}/>
          </div>
       </div>
     );
-  }
-  handleNewQClick() {
-    alert('Get new question button pressed.');
   }
 }
