4a16d1d75804f707d84436e242dc3b8cb3be1267
Donncha O'Toole <dnncha@gmail.com>
2017-08-05 14:29:44 +0100

    the damn quotes

--- src/js/components/NewQuestionButton.js
+++ src/js/components/NewQuestionButton.js
@@ -3,11 +3,10 @@ import React from "react";
 export default class NewQuestionButton extends React.Component {  
   getNewQuestion() {
     alert('Get new question button pressed.');
-    console.log("Get new question button pressed.");
   }
   render() {
     return (
-      <button onClick="{this.getNewQuestion}" class="btn btn-primary">New Question</button>
+      <button onClick={this.getNewQuestion} class="btn btn-primary">New Question</button>
     );
   }
 }
