d5d4860f3ce6d68523172719d1da19d014601881
florn <flapsnms@gmail.com>
2017-08-05 14:12:17 +0100

    Added question state with string arrays for body of question

--- src/js/components/Question.js
+++ src/js/components/Question.js
@@ -1,12 +1,15 @@
 import React from "react";
 
 export default class Question extends React.Component {
-
+  constructor(props) {
+    super(props);
+    this.state = {questionVersions: ['Solve the following integral: $$ \\int_{1}^{0} x^2  $$ Please show all your work.',
+    'Solve the following integral: $$ \\int_{2}^{3} x^6  $$ Please show all your work.']};
+  }
   render() {
     return (
       <div>
-      <p>Define how many apples John has where Mary is cycling away from a giant spinning apple at a rate of 5 m/s and where the apple is red. </p>
-      <div className="maths">{'Solve the following integral: $$ \\int_{a}^{b} x^2  $$ Please show all your work.'}</div>
+      <div className="maths">{this.state.questionVersions[0]}</div>
       </div>
     );
   }
