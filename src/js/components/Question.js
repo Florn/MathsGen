64331ffe8b41419e13418a8a40010053d3649aa3
florn <flapsnms@gmail.com>
2017-08-05 15:14:11 +0100

    minor changes

--- src/js/components/Question.js
+++ src/js/components/Question.js
@@ -14,13 +14,8 @@ export default class Question extends React.Component {
   
   handleNewQClick() {    
     alert('Get new question button pressed.');
-  }
+  };
 
-    this.state = {
-      questionVersions: ['Solve the following integral: $$ \\int_{1}^{0} x^2  $$ Please show all your work.',
-      'Solve the following integral: $$ \\int_{2}^{3} x^6  $$ Please show all your work.']
-    };
-  }
   handleNewQClick() {
     console.log('Get new question button pressed.');
   }
