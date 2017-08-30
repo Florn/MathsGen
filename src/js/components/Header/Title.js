978b9839aff6b4f336653b9f5268d4b1a51d9726
Donncha O'Toole <dnncha@gmail.com>
2017-08-30 13:45:22 +0100

    Animation fun

--- src/js/components/Header/Title.js
+++ src/js/components/Header/Title.js
@@ -3,7 +3,10 @@ import React from "react";
 export default class Title extends React.Component {
   render() {
     return (
+      <div>
       <a class="navbar-brand">{this.props.title}</a>
+      <div class="bar"></div>
+      </div>
     );
   }
 }
--- src/js/components/questions.js
+++ src/js/components/questions.js
@@ -1,9 +1,9 @@
 var DATA = {"questions": {
-    "Integration 2015 Paper 1" : ["$$ \\int_{1}^{0} x^2  $$ This is an integral. Solve now. NOW.",
-    "$$ \\int_{x}^{y} y^-1  $$ This is an integral. Solve now. NOW."], 
+    "Integration 2015 Paper 1" : ["$$ \\int_{1}^{0} x^2  $$ This is an integral. Solve now.",
+    "$$ \\int_{x}^{y} y^-1  $$ This is an integral. Solve now."], 
     "Statistics 2013 Paper 2" : ["Solve the following statistics: $$ {1}^{0}% x^2  $$ Please show all your work.",
     "Solve the following statistics: $$ {1}^{3} + z^3  $$ Please show all your work."],
-    "Integration 2011 Paper 1" : ["$$ {x}^{y} z^2  $$ Prove Donncha's Theorem, also known as The Genius Theorem.",
-    "$$ {z}^{h} 2^2  $$ Prove Donncha's Theorem, also known as The Genius Theorem."]
+    "Integration 2011 Paper 1" : ["$$ {x}^{y} z^2  $$ Prove Alice's Theorem, also known as The Forward Theorem.",
+    "$$ {z}^{h} 2^2  $$ Prove Bobs's Theorem, also known as The Reverse Theorem."]
 }}
 export default DATA
